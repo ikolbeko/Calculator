074a052159659cdd0cb3d634e6be4e5dd0b7ec04
Ilya Kolbeko <kolbeko@yahoo.com>
2022-02-14 23:57:41 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Илья Колбеко on 14.02.22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
